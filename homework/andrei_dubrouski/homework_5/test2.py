fb573d8934b0b5f62ee6adba4a5d724e64b7be68
Andrei Dubrouski <Andrei_Dubrouski@epam.com>
2024-08-22 00:06:03 +0400

    andrei_dubrouski_homework_5 updated4

--- homework/andrei_dubrouski/homework_5/test2.py
+++ homework/andrei_dubrouski/homework_5/test2.py
@@ -1,16 +1,16 @@
-oper_res1, oper_res2, progr_res1 = ['результат операции: 42', 'результат операции: 514', 'результат работы программы: 9']
+op_res1, op_res2, pro_res1 = ['результат операции: 42', 'результат операции: 514', 'результат работы программы: 9']
 
-colon_index1 = oper_res1.index(':')
+colon_index1 = op_res1.index(':')
 
-colon_index2 = oper_res2.index(':')
+colon_index2 = op_res2.index(':')
 
-colon_index3 = progr_res1.index(':')
+colon_index3 = pro_res1.index(':')
 
-number_str1 = oper_res1[colon_index1 + 2:]
+number_str1 = op_res1[colon_index1 + 2:]
 
-number_str2 = oper_res2[colon_index2 + 2:]
+number_str2 = op_res2[colon_index2 + 2:]
 
-number_str3 = progr_res1[colon_index3 + 2:]
+number_str3 = pro_res1[colon_index3 + 2:]
 
 result1 = int(number_str1) + 10
 
