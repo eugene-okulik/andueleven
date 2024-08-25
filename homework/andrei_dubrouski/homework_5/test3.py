920fcd2658e656551f48c252891a354109ac9305
Andrei Dubrouski <Andrei_Dubrouski@epam.com>
2024-08-25 17:44:02 +0400

    andrei_dubrouski_homework_5 updated 3rd task

--- homework/andrei_dubrouski/homework_5/test3.py
+++ homework/andrei_dubrouski/homework_5/test3.py
@@ -1,11 +1,9 @@
 students = ['Ivanov', 'Petrov', 'Sidorov']
 
+text1 = str(', '.join(students))
+
 subjects = ['math', 'biology', 'geography']
 
-sur1, sur2, sur3 = students
+text2 = str(', '.join(subjects))
 
-subj1, subj2, subj3 = subjects
-
-my_text = f'Students {sur1}, {sur2}, {sur3} study these subjects: {subj1}, {subj2}, {subj3}'
-
-print(my_text)
+print(f'Students {text1} study these subjects: {text2}')
