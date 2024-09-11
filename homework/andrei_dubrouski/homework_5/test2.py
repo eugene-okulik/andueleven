973ef32c951f45d1422d3a3f0e24465594f29b21
Andrei Dubrouski <Andrei_Dubrouski@epam.com>
2024-09-11 23:12:53 +0400

    Andrei_Dubrouski homework_6 done

--- homework/andrei_dubrouski/homework_5/test2.py
+++ homework/andrei_dubrouski/homework_5/test2.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 oper_result1, oper_result2, progr_result1 = ['результат операции: 42', 'результат операции: 514', 'результат работы программы: 9']
 
 colon_index1 = oper_result1.index(':')
@@ -11,6 +12,21 @@ number_str1 = oper_result1[colon_index1 + 2:]
 number_str2 = oper_result2[colon_index2 + 2:]
 
 number_str3 = progr_result1[colon_index3 + 2:]
+=======
+op_res1, op_res2, pro_res1 = ['результат операции: 42', 'результат операции: 514', 'результат работы программы: 9']
+
+colon_index1 = op_res1.index(':')
+
+colon_index2 = op_res2.index(':')
+
+colon_index3 = pro_res1.index(':')
+
+number_str1 = op_res1[colon_index1 + 2:]
+
+number_str2 = op_res2[colon_index2 + 2:]
+
+number_str3 = pro_res1[colon_index3 + 2:]
+>>>>>>> 4a4b8f47735cb2a44624295b1ac7f19ca9465666
 
 result1 = int(number_str1) + 10
 
--- homework/andrei_dubrouski/homework_5/test3.py
+++ homework/andrei_dubrouski/homework_5/test3.py
@@ -1,5 +1,6 @@
 students = ['Ivanov', 'Petrov', 'Sidorov']
 
+<<<<<<< HEAD
 subjects = ['math', 'biology', 'geography']
 
 surname1, surname2, surname3 = students
@@ -9,3 +10,12 @@ subject1, subject2, subject3 = subjects
 my_text = f'Students {surname1}, {surname2}, {surname3} study these subjects: {subject1}, {subject2}, {subject3}'
 
 print(my_text)
+=======
+text1 = str(', '.join(students))
+
+subjects = ['math', 'biology', 'geography']
+
+text2 = str(', '.join(subjects))
+
+print(f'Students {text1} study these subjects: {text2}')
+>>>>>>> 4a4b8f47735cb2a44624295b1ac7f19ca9465666
