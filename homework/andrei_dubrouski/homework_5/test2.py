795dd3837e555b48a33f040fd0acdd58c6a560fe
Andrei Dubrouski <Andrei_Dubrouski@epam.com>
2024-09-11 23:29:08 +0400

    Andrei_Dubrouski homework_6 done

--- homework/andrei_dubrouski/homework_5/test2.py
+++ homework/andrei_dubrouski/homework_5/test2.py
@@ -1,17 +1,3 @@
-oper_result1, oper_result2, progr_result1 = ['результат операции: 42', 'результат операции: 514', 'результат работы программы: 9']
-
-colon_index1 = oper_result1.index(':')
-
-colon_index2 = oper_result2.index(':')
-
-colon_index3 = progr_result1.index(':')
-
-number_str1 = oper_result1[colon_index1 + 2:]
-
-number_str2 = oper_result2[colon_index2 + 2:]
-
-number_str3 = progr_result1[colon_index3 + 2:]
-
 op_res1, op_res2, pro_res1 = ['результат операции: 42', 'результат операции: 514', 'результат работы программы: 9']
 
 colon_index1 = op_res1.index(':')
--- homework/andrei_dubrouski/homework_5/test3.py
+++ homework/andrei_dubrouski/homework_5/test3.py
@@ -1,15 +1,5 @@
 students = ['Ivanov', 'Petrov', 'Sidorov']
 
-subjects = ['math', 'biology', 'geography']
-
-surname1, surname2, surname3 = students
-
-subject1, subject2, subject3 = subjects
-
-my_text = f'Students {surname1}, {surname2}, {surname3} study these subjects: {subject1}, {subject2}, {subject3}'
-
-print(my_text)
-
 text1 = str(', '.join(students))
 
 subjects = ['math', 'biology', 'geography']
